1f46789279edbba4d090f4119a2a0bc300bfe221
KenArsen <kenzhegulov2002@gmail.com>
2024-01-12 20:02:57 +0600

    created serializers and urls, changed views

--- card/views.py
+++ card/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import Card
-from .serilizers import CardSerializer
+from .serializers import CardSerializer
 
 
 class CardList(generics.ListAPIView):
