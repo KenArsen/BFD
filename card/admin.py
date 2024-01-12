727ec901ff8b78d6cdb663fffffa7a7d116e814a
KenArsen <kenzhegulov2002@gmail.com>
2024-01-12 20:11:25 +0600

    created serializers and urls, changed views

--- card/admin.py
+++ card/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from card.models import Card
 
 
-@admin.register(Card)
-class CardAdmin(admin.ModelAdmin):
-    list_display = ('lesson_number', 'name')
-    search_fields = ('lesson_number', 'name')
-    list_filter = ('lesson_number', 'name')
-    list_display_links = ('lesson_number',)
+# @admin.register(Card)
+# class CardAdmin(admin.ModelAdmin):
+#     list_display = ('lesson_number', 'name')
+#     search_fields = ('lesson_number', 'name')
+#     list_filter = ('lesson_number', 'name')
+#     list_display_links = ('lesson_number',)
