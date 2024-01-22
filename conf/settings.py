d8894ff6f2d5f010e7186d17f078e10f5026a20a
KenArsen <kenzhegulov2002@gmail.com>
2024-01-22 17:46:11 +0600

    change DEBUG=False

--- conf/settings.py
+++ conf/settings.py
@@ -13,7 +13,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG') == 'True'
-print(DEBUG)
 
 ALLOWED_HOSTS = list(os.getenv('ALLOWED_HOSTS'))
 
