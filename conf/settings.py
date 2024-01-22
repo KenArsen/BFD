183b8d0d00ae0dc6522fe35410464b05a6fc151a
KenArsen <kenzhegulov2002@gmail.com>
2024-01-22 18:35:35 +0600

    added CSRF_COOKIE_SECURE, SESSION_COOKIE_SECURE

--- conf/settings.py
+++ conf/settings.py
@@ -129,3 +129,6 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == "True"
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
