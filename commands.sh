302e0800b820c6f97f3f44d5af4830af73925320
KenArsen <kenzhegulov2002@gmail.com>
2024-01-22 15:30:14 +0600

    ch16

--- commands.sh
+++ commands.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-exec /bin/sh -c "python manage.py migrate && \
-      python manage.py collectstatic --no-input && \
-      gunicorn --bind :8000 conf.wsgi:application"
+
+python manage.py migrate
+
+python manage.py collectstatic --no-input
+
+# Create a user
+python manage.py shell -c "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.get_or_create(username='bfd_user', \
+    defaults={'password': 'bfd_password', 'email': 'bfd@gmail.com', 'is_staff': True, 'is_superuser': True})"
+
+# Start Gunicorn
+exec /bin/sh -c "gunicorn --bind :8000 conf.wsgi:application"
+
 
 chmod +x commands.sh
