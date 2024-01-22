f9ce99d36ac33a0e2b57599866f3cab9d2beadc0
KenArsen <kenzhegulov2002@gmail.com>
2024-01-22 20:29:56 +0600

    add CSRF_TRUSTED_ORIGINS

--- conf/settings.py
+++ conf/settings.py
@@ -129,3 +129,5 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == "True"
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
+
+CSRF_TRUSTED_ORIGINS = ['https://teamone.inclusivetec.com', 'http://127.0.0.1']
